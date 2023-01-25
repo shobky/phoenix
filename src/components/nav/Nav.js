c6ff5c75d1faaa72b650a7f94fab26b453caf498
shobkeyy <shobkyy@gmail.com>
2023-01-25 09:57:13 +0200

    'final-touches'

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,18 +1,17 @@
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.jpeg'
 import { MdEmail } from 'react-icons/md'
 import { RiLinkedinFill } from 'react-icons/ri'
 import { AiFillInstagram } from 'react-icons/ai'
 import './nav.css'
 import { Link } from 'react-router-dom'
-import { FiExternalLink } from 'react-icons/fi'
+import { RxArrowTopRight } from 'react-icons/rx'
 
 const Nav = () => {
     return (
         <nav id="header" className="nav">
-            <div className="nav_logo-section">
-                {/* <img className='nav_logo-img' alt="logo" src={logo} /> */}
-                <h1 className='nav_logo-txt'>SO HYPNOTIZING</h1>
-            </div>
+            {/* <div className="nav_logo-section"> */}
+                <img className='nav_logo-img' alt="logo" src={logo} />
+            {/* </div> */}
             <div className="nav-righ-side">
                 <div className="nav_links-section">
                     <a className="nav-link" href="mailto:newbiz@phoenixagency.ae"><MdEmail className="nav-link_ico" /></a>
@@ -20,7 +19,7 @@ const Nav = () => {
                     <a className="nav-link" href="#j"><RiLinkedinFill className="nav-link_ico" /></a>
                 </div>
                 <Link className="nav_button" to='/form'>
-                    GET IN TOUCH <FiExternalLink/></Link>
+                    Get in touch <RxArrowTopRight/></Link>
             </div>
 
         </nav>
